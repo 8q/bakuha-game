e2eae567c8795d6d5b32dba58840490cb691cb1d
shira <shira7867@gmail.com>
2017-10-29 20:25:22 +0900

    [add]ゲージ

--- Assets/Script/Mic.cs
+++ Assets/Script/Mic.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Mic : MonoBehaviour {
 
 	public GameObject attatchedObj;
+
+    public GameObject gauge;
 
 	public GameObject bakuhatu;
 
@@ -19,22 +22,28 @@ public class Mic : MonoBehaviour {
 	void Update(){
 		float vol = GetAveragedVolume();
 
-		if (vol > 0.1) {
-			var pos = attatchedObj.transform.position;
-			Instantiate (bakuhatu, new Vector3(pos.x, pos.y, pos.z), Quaternion.identity);
-			Destroy (attatchedObj);
-			Destroy (gameObject);
-		}
+        float size = 800 * vol;
+        var rectTransform = gauge.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, size);
+        Debug.Log(rectTransform.localPosition.y);
+        rectTransform.localPosition = new Vector3(rectTransform.localPosition.x, -111 + size / 2, rectTransform.localPosition.z);
+
+		//if (vol > 0.1) {
+		//	var pos = attatchedObj.transform.position;
+		//	Instantiate (bakuhatu, new Vector3(pos.x, pos.y, pos.z), Quaternion.identity);
+		//	Destroy (attatchedObj);
+		//	Destroy (gameObject);
+		//}
 	}
 
 	float GetAveragedVolume(){ 
-		float[] data = new float[256];
+		float[] data = new float[1000];
 		float a = 0;
 		GetComponent<AudioSource> ().GetOutputData(data,0);
 		foreach(float s in data) {
 			a += Mathf.Abs(s);
 		}
-		return a/256.0f;
+		return a/1000.0f;
 	}
 
 }
