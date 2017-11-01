d665eb5af89bd37b16af93ed8000eb932f909b5c
shira <shira7867@gmail.com>
2017-11-01 13:49:41 +0900

    [add]炎とか

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, (Input.GetAxis("Horizontal") * 1), 0);
+
+    }
+}
--- Assets/Script/Mic.cs
+++ Assets/Script/Mic.cs
@@ -11,7 +11,15 @@ public class Mic : MonoBehaviour {
 
 	public GameObject bakuhatu;
 
-	void Start(){
+    public GameObject fire;
+
+    private bool isDeleted = false;
+
+    float vol = 0.0f;
+
+
+	void Start()
+    {
 		var aud = GetComponent<AudioSource> ();
 		aud.clip = Microphone.Start(null, true, 999, 44100);
 		aud.loop = true;
@@ -19,31 +27,54 @@ public class Mic : MonoBehaviour {
 		aud.Play();
 	}
 	
-	void Update(){
-		float vol = GetAveragedVolume();
-
-        float size = 800 * vol;
+	void Update()
+    {
+        if (isDeleted) return;
+        vol = GetAveragedVolume();
+        //vol += 0.001f;
+        float size = 450 * vol;
         var rectTransform = gauge.GetComponent<RectTransform>();
         rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, size);
-        Debug.Log(rectTransform.localPosition.y);
         rectTransform.localPosition = new Vector3(rectTransform.localPosition.x, -111 + size / 2, rectTransform.localPosition.z);
+        int border = 170;
 
-		//if (vol > 0.1) {
-		//	var pos = attatchedObj.transform.position;
-		//	Instantiate (bakuhatu, new Vector3(pos.x, pos.y, pos.z), Quaternion.identity);
-		//	Destroy (attatchedObj);
-		//	Destroy (gameObject);
-		//}
+        if(size <= border)
+        {
+            gauge.GetComponent<Image>().color = new Color(0, 1.0f, 0);
+        }
+        else if (size > border)
+        {
+            if (size > 223.0f) size = 223.0f;
+            gauge.GetComponent<Image>().color = new Color(1.0f, 0, 0);
+            isDeleted = true;
+            StartCoroutine(Bakuhatu());
+        }
 	}
 
-	float GetAveragedVolume(){ 
+	float GetAveragedVolume()
+    { 
 		float[] data = new float[1000];
 		float a = 0;
 		GetComponent<AudioSource> ().GetOutputData(data,0);
-		foreach(float s in data) {
+		foreach(float s in data)
+        {
 			a += Mathf.Abs(s);
 		}
 		return a/1000.0f;
 	}
+
+    IEnumerator Bakuhatu()
+    {
+        var pos = attatchedObj.transform.position;
+        float range = 30.0f;
+        for(int i = 0; i < 3; i++)
+        {
+            Instantiate(bakuhatu, new Vector3(pos.x + Random.Range(-range, range), pos.y, pos.z + Random.Range(-range, range)), Quaternion.identity);
+            yield return new WaitForSeconds(0.2f);
+        }
+        fire.SetActive(true);
+        Destroy(attatchedObj);
+        Destroy(gameObject);
+    }
 
 }
