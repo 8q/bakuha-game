5cd6ecea88ec4a369b44f9ca6affcd97f71abd33
shira <shira7867@gmail.com>
2017-11-02 18:58:18 +0900

    [add]Spaceで浮かぶ

--- Assets/Script/MyCamera.cs
+++ Assets/Script/MyCamera.cs
@@ -22,7 +22,7 @@ public class MyCamera : MonoBehaviour {
             child.position.x + direction * child.position.normalized.x, 
             child.position.y, 
             child.position.z + direction * child.position.normalized.z);
-        if(newPosition.magnitude > 30 && newPosition.magnitude < 80)
+        if(new Vector3(newPosition.x, 0, newPosition.z).magnitude > 30 && new Vector3(newPosition.x, 0, newPosition.z).magnitude < 80)
         {
             child.position = newPosition;
         }
@@ -31,5 +31,16 @@ public class MyCamera : MonoBehaviour {
         if (Input.GetKey(KeyCode.UpArrow)) pitch = -0.8f;
         else if (Input.GetKey(KeyCode.DownArrow)) pitch = 0.8f;
         child.transform.Rotate(Quaternion.AngleAxis(0.001f, new Vector3(child.position.x, 0, child.position.z).normalized) * new Vector3(pitch, 0, 0));
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(0, 0.5f, 0);
+            if (transform.position.y > 100) transform.position = new Vector3(transform.position.x, 100, transform.position.z);
+        }
+        else if(transform.position.y > 0)
+        {
+            transform.Translate(0, -1f, 0);
+            if (transform.position.y < 0) transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
     }
 }
