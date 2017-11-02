a46da54b57941bc66c082fe47269aa25b8ca41be
shira <shira7867@gmail.com>
2017-11-02 16:13:42 +0900

    [add]前後移動

--- Assets/Script/MyCamera.cs
+++ Assets/Script/MyCamera.cs
@@ -12,6 +12,15 @@ public class MyCamera : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.Rotate(0, (Input.GetAxis("Horizontal") * -1), 0);
-
+        var child = transform.Find("Main Camera");
+        float verticalInput = -Input.GetAxis("Vertical");
+        var newPosition =  new Vector3(
+            child.position.x + verticalInput * child.position.normalized.x, 
+            child.position.y, 
+            child.position.z + verticalInput * child.position.normalized.z);
+        if(newPosition.magnitude > 30 && newPosition.magnitude < 80)
+        {
+            child.position = newPosition;
+        }
     }
 }
